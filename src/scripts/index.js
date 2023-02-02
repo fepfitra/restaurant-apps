4c422192cf8c0efcc0e917c4c484cb7b4162f0fe
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2023-02-02 12:00:46 -0500

    with temporary background, error text-elipsis, and no styled footer
    	new file:   node_modules/.package-lock.json
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/scripts/index.js
    	deleted:    src/styles/main.css
    	new file:   src/styles/main.scss
    	new file:   src/styles/module/_content.scss
    	new file:   src/styles/module/_header.scss
    	new file:   src/styles/module/_hero.scss
    	new file:   src/styles/module/_reset.scss
    	modified:   src/templates/index.html
    	modified:   webpack.common.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,19 @@
 import 'regenerator-runtime'; /* for async await transpile */
-import '../styles/main.css';
+import '../styles/main.scss';
+import data from '../DATA.json';
 
-console.log('Hello Coders! :)');
+const {restaurants} = data;
+
+const posts = document.querySelector(".posts");
+
+restaurants.forEach((restaurant) => {
+  posts.innerHTML += `
+    <article class="post-item">
+      <img class="post-item__thumbnail"
+      src="${restaurant.pictureId}">
+      <h2 class="resto__title">${restaurant.name} - ${restaurant.city}</h2>
+      <h3 class="resto__rating">Rating - ${restaurant.rating}</h3>
+      <p class="resto__desc">${restaurant.description}</p>
+    </article>
+  `;
+});
