44e74cca717c59e20b8e1005d5c965cfe2af50a8
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2023-02-08 06:33:28 -0500

    media still down work

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,6 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.scss';
+import '../styles/main.scss';
 import data from '../DATA.json';
 
 const {restaurants} = data;
