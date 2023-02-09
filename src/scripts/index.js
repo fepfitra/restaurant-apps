efedbe4abcbe7e5c7aea3b52be5537fe14d1cf96
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2023-02-08 21:05:42 -0500

    done @media sceen

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,11 +1,15 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.scss';
-import '../styles/main.scss';
+import '../styles/responsive.scss';
 import data from '../DATA.json';
 
 const {restaurants} = data;
 
 const posts = document.querySelector(".posts");
+const menu = document.querySelector("#menu");
+const hero = document.querySelector('.hero');
+const main = document.querySelector('main');
+const drawer = document.querySelector('#drawer');
 
 restaurants.forEach((restaurant) => {
   posts.innerHTML += `
@@ -18,3 +22,18 @@ restaurants.forEach((restaurant) => {
     </article>
   `;
 });
+
+// menu.addEventListener('click', (event) => {
+//   drawer.classList.toggle('open');
+//   event.stopPropagation();
+// });
+
+// hero.addEventListener('click', (event) => {
+//   drawer.classList.remove('open');
+//   event.stopPropagation();
+// });
+
+// main.addEventListener('click', (event) => {
+//   drawer.classList.remove('open');
+//   event.stopPropagation();
+// });
