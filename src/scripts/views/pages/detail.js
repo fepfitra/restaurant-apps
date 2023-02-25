1424805474b067f977fce9a277794969a192bbf8
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2023-02-25 17:12:50 +0700

    design for detail later

--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -5,13 +5,7 @@ import { createRestaurantDetailTemplate } from '../templates/template-creator';
 const Detail = {
   async render() {
     return `
-      <section class="content">
-        <div class="restos">
-          <h1 class="restos__label">Detail</h1>
-          <div class="posts" id="maincontent" tabindex="0">
-
-          </div>
-        </div>
+      <section class="content" tabindex="0">
       </section>
     `;
   },
@@ -19,8 +13,9 @@ const Detail = {
   async afterRender() {
     const url = UrlParser.parseActiveUrlWithoutCombiner();
     const restaurant = await RestaurantsSource.detailRestaurant(url.id);
-    const restaurantContainer = document.querySelector('#maincontent');
+    const restaurantContainer = document.querySelector('.content');
     restaurantContainer.innerHTML = createRestaurantDetailTemplate(restaurant.restaurant);
+    console.log(restaurant.restaurant);
   },
 };
 
--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -1,8 +1,15 @@
 import CONFIG from '../../globals/config';
 
 const createRestaurantDetailTemplate = (restaurant) => `
+  <article class="detail-item" tabindex="0">
+    <img class="detail-item__thumbnail"
+    src="${CONFIG.BASE_URL + CONFIG.MEDIUM_PICTURE + restaurant.pictureId}"
+    alt= "${restaurant.description}">
+    <h2 class="resto__title"><a href="#/detail/${restaurant.id}">${restaurant.name} - ${restaurant.city}</a></h2>
+    <h3 class="resto__rating">Rating - ${restaurant.rating}</h3>
+  <h3 class="resto__rating"> 
   ${restaurant.address}
-  ${restaurant.categories[0]}
+  ${restaurant.categories[0].name}
   ${restaurant.categories[1]}
   ${restaurant.city}
   ${restaurant.customerReviews[0]}
@@ -11,17 +18,18 @@ const createRestaurantDetailTemplate = (restaurant) => `
   ${restaurant.menus[0]}
   ${restaurant.name}
   ${restaurant.pictureId}
-  ${restaurant.rating}
+  ${restaurant.rating} </h3>
+  </article>
 `;
 
 
 const createRestaurantItemTemplate = (restaurant) => `
   <article class="post-item" tabindex="0">
     <img class="post-item__thumbnail"
-    src="${CONFIG.BASE_URL + CONFIG.MEDIUM_PICTURE + restaurant.pictureId}"
+    src="${CONFIG.BASE_URL + CONFIG.SMALL_PICTURE + restaurant.pictureId}"
     alt= "${restaurant.description}">
-    <h2 class="resto__title">${restaurant.name} - ${restaurant.city}</h2>
-    <h3 class="resto__rating">Rating - ${restaurant.rating}</h3>
+    <h2 class="resto__title"><a href="#/detail/${restaurant.id}">${restaurant.name} - ${restaurant.city}</a></h2>
+    <h3 class="resto__rating">⭐️ ${restaurant.rating}</h3>
   </article>
 `;
 
