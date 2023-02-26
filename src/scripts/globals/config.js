fb304f1a042ef6a5d6e01c09bd9ce950ddd7a62c
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2023-02-26 13:04:42 +0700

    preparing DB operatoions function

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -3,6 +3,9 @@ const CONFIG = {
   SMALL_PICTURE: 'images/small/',
   MEDIUM_PICTURE: 'images/medium/',
   LARGE_PICTURE: 'images/large/',
+  DATABASE_NAME: 'night-city-food',
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: 'restaurants',
 };
 
 export default CONFIG;
