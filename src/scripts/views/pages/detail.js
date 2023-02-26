a7acbb58b2d7befc5d86e4429ff289b24f466f27
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2023-02-26 13:15:11 +0700

    add like button in detail page

--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -1,12 +1,12 @@
 import UrlParser from '../../routes/url-parser';
 import RestaurantsSource from '../../data/restaurants-source';
-import { createRestaurantDetailTemplate } from '../templates/template-creator';
+import { createRestaurantDetailTemplate, createLikeButtonTemplate } from '../templates/template-creator';
 
 const Detail = {
   async render() {
     return `
-      <section class="content" tabindex="0">
-      </section>
+      <section class="content" tabindex="0"></section>
+      <div id="likeButtonContainer"></div>
     `;
   },
 
@@ -14,7 +14,10 @@ const Detail = {
     const url = UrlParser.parseActiveUrlWithoutCombiner();
     const restaurant = await RestaurantsSource.detailRestaurant(url.id);
     const restaurantContainer = document.querySelector('.content');
+    const likeButtonContainer = document.querySelector('#likeButtonContainer');
+
     restaurantContainer.innerHTML = createRestaurantDetailTemplate(restaurant.restaurant);
+    likeButtonContainer.innerHTML = createLikeButtonTemplate();
   },
 };
 
--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -86,4 +86,16 @@ const createRestaurantItemTemplate = (restaurant) => `
   </article>
 `;
 
-export { createRestaurantDetailTemplate, createRestaurantItemTemplate };
+const createLikeButtonTemplate = () => `
+  <button aria-label="like this movie" id="likeButton" class="like">
+    <i class="fa fa-heart" aria-hidden="true"></i>
+  </button>
+`;
+
+const createLikedButtonTemplate = () => `
+  <button aria-label="like this movie" id="likeButton" class="like">
+    <i class="fa fa-heart-o" aria-hidden="true"></i>
+  </button>
+`;
+
+export { createRestaurantDetailTemplate, createRestaurantItemTemplate, createLikeButtonTemplate, createLikedButtonTemplate };
