ce4a719285e9088bf0a84ade7ec4ab5bbaf1a6d4
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2023-02-26 14:03:21 +0700

    implement cache in service worker

--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -1,18 +1,22 @@
+import CacheHelper from "./utils/cache-helper";
+
+const assetToCache = [
+  './',
+  './favicon.png',
+  './index.html',
+  './app.bundle.js',
+  './app.manifest',
+  './sw.bundle.js',
+];
+
 self.addEventListener('install', (event) => {
-  console.log('Installing Service Worker ...');
- 
-  // TODO: Caching App Shell Resource
+  event.waitUntil(CacheHelper.cachingAppShell([...assetToCache]));
 });
  
 self.addEventListener('activate', (event) => {
-  console.log('Activating Service Worker ...');
- 
-  // TODO: Delete old caches
+  event.waitUntil(CacheHelper.deleteOldCache());
 });
  
 self.addEventListener('fetch', (event) => {
-  console.log(event.request);
- 
   event.respondWith(fetch(event.request));
-  // TODO: Add/get fetch request to/from caches
 });
